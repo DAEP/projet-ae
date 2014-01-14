143f8a7e5510971beac31e0dcbe6a6e556ffe472
Yohann VAUTRIN <y.vautrin@isae.fr>
2014-01-14 10:45:10 +0100

    Changed main.c to have a default configuration

--- code_C_seq/src/main.c
+++ code_C_seq/src/main.c
@@ -17,14 +17,13 @@ int main(int argc, char *argv[])
     }
 
     problem_set_consts(pb, 20.0e-6);
-//    problem_set_mesh(pb, 0.1, 100, 0.1, 1);
     problem_set_mesh(pb, 0.1, 100, 0.1, 100);
     problem_set_tempo(pb, 1., 100);
     problem_set_init_cond(pb, 100.);
     problem_set_bnd(pb, 0, 1, 1000.);
     problem_set_bnd(pb, 1, 1, 1000.);
-    problem_set_bnd(pb, 2, 0, 0.);
-    problem_set_bnd(pb, 3, 0, 0.);
+    problem_set_bnd(pb, 2, 1, 1000.);
+    problem_set_bnd(pb, 3, 1, 1000.);
     problem_check(pb);
 
     visual_write_case(pb);
