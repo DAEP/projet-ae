eb7db2c910c0f4ad8e4867acfee1527216afd067
Yohann VAUTRIN <y.vautrin@isae.fr>
2014-01-14 15:30:03 +0100

    Added a configuration file for the simulations

--- code_C_seq/src/main.c
+++ code_C_seq/src/main.c
@@ -9,6 +9,7 @@ int main(int argc, char *argv[])
      * Constants, mesh definition and time discretization
      */
     problem_t *pb = problem_create();
+	FILE *fp_config = NULL;
 
     if(pb == NULL)
     {
@@ -16,16 +17,18 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    problem_set_consts(pb, 20.0e-6);
-//    problem_set_mesh(pb, 0.1, 100, 0.1, 1);
-    problem_set_mesh(pb, 0.1, 100, 0.1, 100);
-    problem_set_tempo(pb, 1., 100);
-    problem_set_init_cond(pb, 100.);
-    problem_set_bnd(pb, 0, 1, 1000.);
-    problem_set_bnd(pb, 1, 1, 1000.);
-    problem_set_bnd(pb, 2, 0, 0.);
-    problem_set_bnd(pb, 3, 0, 0.);
+	fp_config = fopen(argv[1], "r");
+	if (fp_config == NULL)
+    {
+		fprintf(stderr, "Could not open the configuration file!\n");
+        return EXIT_FAILURE;
+    }
+
+	problem_config(fp_config, pb);
+
     problem_check(pb);
+
+	fclose(fp_config);
 
     visual_write_case(pb);
     visual_write_geo(pb);
